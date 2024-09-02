7359b35d108c2bd7bb5c028cca58959469795abc
mkittyhartono <magdakittyhartono@gmail.com>
2024-09-02 21:06:06 +0700

    update code

---
+++ test/specs/test.demo.js
@@ -0,0 +1,18 @@
+describe('Test Saucedemo', () => {
+    it('Test 1 - Login Succesfully',async () => {
+        await browser.url('https://www.saucedemo.com/');
+
+        const userNameTextBox = await browser.$('#user-name');
+        const passwordTextBox = await browser.$('#password');
+        const loginButton = await browser.$('//input[@type="submit"]');
+
+        await userNameTextBox.addValue('standard_user');
+        await passwordTextBox.addValue('secret_sauce');
+        await loginButton.click();
+
+        await browser.pause(5000);
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+        await expect(browser).toHaveTitle('Swag Labs');
+
+    });
+});
